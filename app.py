f1bb08cdbd64d7a5c39d532e896134620bc3649f
Gabriel Medeiros <104704902+medeiroos@users.noreply.github.com>
2022-05-22 08:30:37 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -3,22 +3,50 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/<int:id>')
-def pessoas(id):
-    return jsonify({'id': id, 'nome': 'Gabriel', 'profissao': 'Desenvolvedor'})
+desenvolvedores = [
+    {
+        'id':'0',
+        'nome': 'Gabriel',
+        'habilidaddes': ['Python', 'Flask']
+     },
+    {
+        'id':'1',
+        'nome': 'Rafael',
+        'habilidades': ['Python', 'Django']
+     }
+]
 
-#@app.route('/soma/<int:valor1>/<int:valor2>/')
-#def soma(valor1, valor2):
-#    return jsonify({'soma': valor1 + valor2})
+# DEVOLVE UM DESENVOLVEDOR PELO ID, TAMBEM ALTERA E DELETA UM DESENVOLVEDOR
+@app.route('/dev/<int:id>/', methods=['GET', 'PUT', 'DELETE'])
+def desenvolvedor(id):
+    if request.method == 'GET':
+        try:
+            response = desenvolvedores[id]
+        except IndexError:
+            response = {'status': 'ERRO', 'mensagem':'Desenvolvedor de ID {} não existe'.format(id)}
+        except Exception:
+            mensagem = 'Erro desconhecido. Procure o administrador da API'
+            response = {'status': 'ERRO', 'mensagem': mensagem}
+        return jsonify(response)
+    elif request.method == 'PUT':
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+        return jsonify(dados)
+    elif request.method == 'DELETE':
+        desenvolvedores.pop(id)
+        return jsonify({'status': 'sucesso', 'mensagem': 'Registro excluído!'})
 
-@app.route('/soma', methods=['POST', 'GET'])
-def soma():
+# LISTA TODOS OS DESENVOLVEDORES E TAMBEM CRIA UM NOVO DESENVOLVEDOR
+@app.route('/dev/', methods=['POST', 'GET'])
+def lista_desenvolvedores():
     if request.method == 'POST':
         dados = json.loads(request.data)
-        total = sum(dados['valores'])
+        posicao = len(desenvolvedores)
+        dados['id'] = posicao
+        desenvolvedores.append(dados)
+        return jsonify(desenvolvedores[posicao])
     elif request.method == 'GET':
-        total = 10 + 10
-    return jsonify({'soma': total})
+        return jsonify(desenvolvedores)
 
 if __name__ == '__main__':
     app.run(debug=True)
